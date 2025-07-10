91b3c806133333a7f2244e021d222b80f5b36cd6
Amit Yadav <ay3106222@gmail.com>
2025-07-10 10:06:58 +0530

    Add Array003 define scanner properties for take input to user and store element in java array then show the output using loops for loops method

---
+++ src/DSA_Array/Array005.java
@@ -0,0 +1,30 @@
+package DSA_Array;
+
+import java.util.Scanner;
+
+public class Array005 {
+    public static void main(String[] args) {
+        //Define Scanner Class
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Array Length Number:-");
+        int ln=sc.nextInt();
+        System.out.println(" ");
+
+        //define Array for store data
+        int[] arr=new int[ln];
+
+        //define Array for how to insert data by using loops method
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter arr of index"+i+"to Store:-");
+            arr[i]=sc.nextInt();
+        }
+
+        //for Break line of Input line
+        System.out.println();
+        System.out.println("======Output is Here========");
+        System.out.println(" ");
+        for (int x:arr){
+            System.out.println("Array List Item is:-"+x);
+        }
+    }
+}
