9a1bc989ee46cc1fff349636d7e30e2356a40312
Amit Yadav <ay3106222@gmail.com>
2025-07-10 17:00:49 +0530

    Practice code for how define Swap of a number in java programming language and using swap number

---
+++ src/ExtraDefineCode/Swap003.java
@@ -0,0 +1,21 @@
+package ExtraDefineCode;
+
+public class Swap003 {
+    public static void main(String[] args) {
+        //java swap programme 003
+        int temp;
+        int a=20;
+        int b=40;
+
+        temp=a;
+        a=b;
+        b=temp;
+//
+//        temp=b;
+//        b=a;
+//        a=temp;
+        System.out.println("After Swaping number a is:-"+a); //a=20
+        System.out.println("After Swaping number b is:-"+b);    //b=40
+        System.out.println(temp);
+    }
+}
